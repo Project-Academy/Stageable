5a14a0cbaf91db4d228f1a2981670d72e8682177
Saf <136665878+project-saf@users.noreply.github.com>
2026-03-31 16:05:04 +1100

    Change inDuration and outDuration to open access

--- Sources/Stageable/StageVC.swift
+++ Sources/Stageable/StageVC.swift
@@ -40,8 +40,8 @@ open class StageVC: UIViewController {
      */
     open var props: [Prop] = []
 
- 	public var inDuration: TimeInterval = 0.45
-	public var outDuration: TimeInterval = 0.35
+ 	open var inDuration: TimeInterval = 0.45
+	open var outDuration: TimeInterval = 0.35
 
     //--------------------------------------
     // MARK: - TRANSITIONS -
